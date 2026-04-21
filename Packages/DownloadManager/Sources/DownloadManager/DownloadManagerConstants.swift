005f7a9294dd513ef09cbefe990f04e1ef1828a5
Piotr Olechnowicz <olechnowicz.p@gmail.com>
2026-04-21 06:42:42 +0200

    Add a way to fetch all downloadTasks from URLSession

---
+++ Packages/DownloadManager/Sources/DownloadManager/DownloadManagerConstants.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public struct DownloadManagerConstants {
+    public static let backgroundSessionIdentifier = "com.async-download.background"
+}
--- Packages/DownloadManager/Sources/DownloadManager/Network/URLSessionProtocol.swift
+++ Packages/DownloadManager/Sources/DownloadManager/Network/URLSessionProtocol.swift
@@ -3,6 +3,15 @@ import Foundation
 // sourcery: AutoMockable
 protocol URLSessionProtocol {
     func downloadTask(with url: URL) -> URLSessionDownloadTask
+    func allDownloadTasks() async -> [URLSessionDownloadTask]
 }
 
-extension URLSession: URLSessionProtocol {}
+extension URLSession: URLSessionProtocol {
+    func allDownloadTasks() async -> [URLSessionDownloadTask] {
+        await withCheckedContinuation { continuation in
+            getTasksWithCompletionHandler { _, _, downloadTasks in
+                continuation.resume(returning: downloadTasks)
+            }
+        }
+    }
+}
